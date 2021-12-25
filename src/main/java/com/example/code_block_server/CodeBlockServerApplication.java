42afce0118961801d7734a0d387355aa05dca176
Mark Marcello <mmarcello@nuvalence.io>
2021-12-25 18:16:23 -0500

    initial commit & bootstrapping

---
+++ src/main/java/com/example/code_block_server/CodeBlockServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.code_block_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBlockServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeBlockServerApplication.class, args);
+    }
+
+}
