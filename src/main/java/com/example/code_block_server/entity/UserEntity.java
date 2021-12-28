34763093f636046221026103bcc17f588d79aaa7
Mark Marcello <mmarcello@nuvalence.io>
2021-12-27 19:52:43 -0500

    added first 3 entities

---
+++ src/main/java/com/example/code_block_server/entity/UserEntity.java
@@ -0,0 +1,43 @@
+package com.example.code_block_server.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+// name of table
+@Entity(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "created_At")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @OneToMany(mappedBy = "user")
+    private List<TopicArticleEntity> topicArticleEntities;
+}
