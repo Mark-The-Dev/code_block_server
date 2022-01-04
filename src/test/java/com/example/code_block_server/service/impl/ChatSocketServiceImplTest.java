17b22a29ee0a78b7deb6a0d4d27583a9f1c74b1b
Mark Marcello <mmarcello@nuvalence.io>
2022-01-03 23:07:56 -0800

    Added unit tests

--- src/test/java/com/example/code_block_server/service/impl/ChatSocketServiceImplTest.java
+++ src/test/java/com/example/code_block_server/service/impl/ChatSocketServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.example.code_block_server.service.impl;
 
+import com.example.code_block_server.dto.ClientMessageDTO;
 import com.example.code_block_server.entity.ChatMessageEntity;
 import com.example.code_block_server.entity.ChatRoomEntity;
 import com.example.code_block_server.entity.UserEntity;
@@ -7,15 +8,19 @@ import com.example.code_block_server.repository.ChatMessageRepository;
 import com.example.code_block_server.repository.ChatRoomRepository;
 import com.example.code_block_server.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class ChatSocketServiceImplTest {
 
     @InjectMocks
@@ -45,7 +50,7 @@ public class ChatSocketServiceImplTest {
         ChatRoomEntity chatRoomEntity = new ChatRoomEntity();
         chatRoomEntity.setId(15L);
 
-        when(userRepository.getById(12L)).thenReturn(userEntity);
+        when(userRepository.findById(12L)).thenReturn(userEntity);
         when(chatRoomRepository.findById(15L)).thenReturn(chatRoomEntity);
 
         String messageToSend = "Hello from the other sideeeee!";
@@ -56,6 +61,33 @@ public class ChatSocketServiceImplTest {
         ChatMessageEntity capturedMessage = chatMessageCaptor.getValue();
         assertEquals(capturedMessage.getMessage(), "Hello from the other sideeeee!");
         assertEquals(capturedMessage.getUser().getFirstName(), "T-bone");
+    }
+
+    @Test
+    public void processMessageDTO_shouldCreateClientMessage() {
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(12);
+        userEntity.setFirstName("T-bone");
+        userEntity.setLastName("FreshSqueeze");
+
+        ChatRoomEntity chatRoomEntity = new ChatRoomEntity();
+        chatRoomEntity.setId(15L);
+
+        when(userRepository.findById(12L)).thenReturn(userEntity);
+        when(chatRoomRepository.findById(15L)).thenReturn(chatRoomEntity);
+
+        String messageToSend = "Hello from the other sideeeee!";
+
+        ClientMessageDTO clientMessageDTO = chatSocketService.processMessageDTO(12L, 15L, messageToSend);
+
+        verify(chatMessageRepository).save(chatMessageCaptor.capture());
+        ChatMessageEntity capturedMessage = chatMessageCaptor.getValue();
+        assertEquals(capturedMessage.getMessage(), "Hello from the other sideeeee!");
+        assertEquals(capturedMessage.getUser().getFirstName(), "T-bone");
+
+        assertEquals(clientMessageDTO.getMessage(), "Hello from the other sideeeee!");
+        assertEquals(clientMessageDTO.getUserFrom(), "T-bone FreshSqueeze");
     }
 
 }
